f4f192e7e699373c925c7f5d9067c606913a7b4d
Amari Coleman <99526785+amcolem@users.noreply.github.com>
2024-02-11 19:29:56 -0500

    Add files via upload

--- src/main/java/Business/Cart.java
+++ src/main/java/Business/Cart.java
@@ -1,4 +1,32 @@
-package Business;
+package BusinessObjects;
+
+import java.util.ArrayList;
 
 public class Cart {
+    ArrayList<CartProducts> cartProductList = new ArrayList<>();
+
+    public ArrayList<CartProducts> getProductList() {
+        return cartProductList;
+    }
+
+    public void setCartProductList(ArrayList<CartProducts> cartProductList) {
+        this.cartProductList = cartProductList;
+    }
+
+    public void addCartProduct(CartProducts product) {
+        cartProductList.add(product);
+    }
+
+    public void removeCartProduct(CartProducts product) {
+        cartProductList.remove(product);
+    }
+
+    public double getTotalPrice() {
+        double sum = 0;
+
+        for (CartProducts product : cartProductList) {
+            sum = sum + product.getTotalPrice();
+        }
+        return sum;
+    }
 }
